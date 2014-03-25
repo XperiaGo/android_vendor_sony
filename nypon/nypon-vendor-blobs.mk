0843a96e1ccb5d02c1ce37ed434c8380c9c6173d
95A31 <x95a31x@gmail.com>
2014-03-25 09:51:56 +0100

    Cleanup completed

--- nypon/nypon-vendor-blobs.mk
+++ nypon/nypon-vendor-blobs.mk
@@ -18,70 +18,26 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nypon/proprietary/app/HdmiService.apk:system/app/HdmiService.apk \
     vendor/sony/nypon/proprietary/bin/hdmiproxyd:system/bin/hdmiproxyd \
     vendor/sony/nypon/proprietary/bin/hdmi_service_st:system/bin/hdmi_service_st \
-    vendor/sony/nypon/proprietary/etc/AT/manuf_id.cfg:system/etc/AT/manuf_id.cfg \
     vendor/sony/nypon/proprietary/etc/AT/model_id.cfg:system/etc/AT/model_id.cfg \
     vendor/sony/nypon/proprietary/etc/AT/system_id.cfg:system/etc/AT/system_id.cfg \
     vendor/sony/nypon/proprietary/etc/firmware/av8100.fw:system/etc/firmware/av8100.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/cg2900_fm_bt_src_coeff_info.fw:system/etc/firmware/cg2900_fm_bt_src_coeff_info.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/cg2900_fm_ext_src_coeff_info.fw:system/etc/firmware/cg2900_fm_ext_src_coeff_info.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/cg2900_fm_fm_coeff_info.fw:system/etc/firmware/cg2900_fm_fm_coeff_info.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/cg2900_fm_fm_prog_info.fw:system/etc/firmware/cg2900_fm_fm_prog_info.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/CG29XX_0200_0000_patch.fw:system/etc/firmware/CG29XX_0200_0000_patch.fw \
     vendor/sony/nypon/proprietary/etc/firmware/CG29XX_0200_0000_settings.fw:system/etc/firmware/CG29XX_0200_0000_settings.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/FM_FW_CG2910_1_0_P1_4_lut.fw:system/etc/firmware/FM_FW_CG2910_1_0_P1_4_lut.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/FM_FW_CG2910_1_0_P1_4_lut_info.fw:system/etc/firmware/FM_FW_CG2910_1_0_P1_4_lut_info.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/FM_FW_CG2910_1_0_P1_4_prog.fw:system/etc/firmware/FM_FW_CG2910_1_0_P1_4_prog.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/FM_FW_CG2910_1_0_P1_4_prog_info.fw:system/etc/firmware/FM_FW_CG2910_1_0_P1_4_prog_info.fw \
     vendor/sony/nypon/proprietary/etc/firmware/incell_fw_120hz.img:system/etc/firmware/incell_fw_120hz.img \
     vendor/sony/nypon/proprietary/etc/firmware/incell_fw_default.img:system/etc/firmware/incell_fw_default.img \
-    vendor/sony/nypon/proprietary/etc/firmware/R1f.2_SoC1v20_BT_src_coeff_1.1.fw:system/etc/firmware/R1f.2_SoC1v20_BT_src_coeff_1.1.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R1f.2_SoC1v20_Cobra_FM_SOC1_coef.fw:system/etc/firmware/R1f.2_SoC1v20_Cobra_FM_SOC1_coef.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R1f.2_SoC1v20_Cobra_FM_SOC1_prog.fw:system/etc/firmware/R1f.2_SoC1v20_Cobra_FM_SOC1_prog.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R1f.2_SoC1v20_EXT_src_coeff_1.1.fw:system/etc/firmware/R1f.2_SoC1v20_EXT_src_coeff_1.1.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R2c.2_SoC2v06_BT_src_coeff_1.1.fw:system/etc/firmware/R2c.2_SoC2v06_BT_src_coeff_1.1.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R2c.2_SoC2v06_EXT_src_coeff_1.1.fw:system/etc/firmware/R2c.2_SoC2v06_EXT_src_coeff_1.1.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R2c.2_SoC2v06_Cobra_FM_SOC2_coef.fw:system/etc/firmware/R2c.2_SoC2v06_Cobra_FM_SOC2_coef.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/R2c.2_SoC2v06_Cobra_FM_SOC2_prog.fw:system/etc/firmware/R2c.2_SoC2v06_Cobra_FM_SOC2_prog.fw \
-    vendor/sony/nypon/proprietary/etc/firmware/sdd_10.bin:system/etc/firmware/sdd_10.bin \
-    vendor/sony/nypon/proprietary/etc/firmware/sdd_11.bin:system/etc/firmware/sdd_11.bin \
-    vendor/sony/nypon/proprietary/etc/firmware/sdd_20.bin:system/etc/firmware/sdd_20.bin \
     vendor/sony/nypon/proprietary/etc/firmware/sdd_22.bin:system/etc/firmware/sdd_22.bin \
-    vendor/sony/nypon/proprietary/etc/firmware/wsm_10.bin:system/etc/firmware/wsm_10.bin \
-    vendor/sony/nypon/proprietary/etc/firmware/wsm_11.bin:system/etc/firmware/wsm_11.bin \
-    vendor/sony/nypon/proprietary/etc/firmware/wsm_20.bin:system/etc/firmware/wsm_20.bin \
-    vendor/sony/nypon/proprietary/etc/firmware/wsm_22.bin:system/etc/firmware/wsm_22.bin \
     vendor/sony/nypon/proprietary/etc/permissions/com.sonyericsson.privateapis.xml:system/etc/permissions/com.sonyericsson.privateapis.xml \
     vendor/sony/nypon/proprietary/framework/com.sonyericsson.privateapis_impl.jar:system/framework/com.sonyericsson.privateapis_impl.jar \
-    vendor/sony/nypon/proprietary/lib/hw/camera.montblanc.so:system/lib/hw/camera.montblanc.so \
     vendor/sony/nypon/proprietary/lib/hw/hdmi.default.so:system/lib/hw/hdmi.default.so \
     vendor/sony/nypon/proprietary/lib/hw/nfc.montblanc.so:system/lib/hw/nfc.montblanc.so \
     vendor/sony/nypon/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
     vendor/sony/nypon/proprietary/lib/libals.so:system/lib/libals.so \
-    vendor/sony/nypon/proprietary/lib/libcald_client.so:system/lib/libcald_client.so \
     vendor/sony/nypon/proprietary/lib/libcald_hal.so:system/lib/libcald_hal.so \
-    vendor/sony/nypon/proprietary/lib/libcald_imageutil.so:system/lib/libcald_imageutil.so \
-    vendor/sony/nypon/proprietary/lib/libcald_pal.so:system/lib/libcald_pal.so \
-    vendor/sony/nypon/proprietary/lib/libcald_server.so:system/lib/libcald_server.so \
-    vendor/sony/nypon/proprietary/lib/libcald_util.so:system/lib/libcald_util.so \
     vendor/sony/nypon/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
-    vendor/sony/nypon/proprietary/lib/libcamera_clientsemc.so:system/lib/libcamera_clientsemc.so \
-    vendor/sony/nypon/proprietary/lib/libcameraextensionclient.so:system/lib/libcameraextensionclient.so \
-    vendor/sony/nypon/proprietary/lib/libcameraextensionjni.so:system/lib/libcameraextensionjni.so \
-    vendor/sony/nypon/proprietary/lib/libcameraextensionservice.so:system/lib/libcameraextensionservice.so \
     vendor/sony/nypon/proprietary/lib/libcameralight.so:system/lib/libcameralight.so \
     vendor/sony/nypon/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
-    vendor/sony/nypon/proprietary/lib/libcflashdriver.so:system/lib/libcflashdriver.so \
-    vendor/sony/nypon/proprietary/lib/libface.so:system/lib/libface.so \
     vendor/sony/nypon/proprietary/lib/libhdmi_service.so:system/lib/libhdmi_service.so \
     vendor/sony/nypon/proprietary/lib/libhdmiservicejni.so:system/lib/libhdmiservicejni.so \
     vendor/sony/nypon/proprietary/lib/libkeyctrl.so:system/lib/libkeyctrl.so \
-    vendor/sony/nypon/proprietary/lib/libsys-utils.so:system/lib/libsys-utils.so \
-    vendor/sony/nypon/proprietary/usr/share/camera/config_file/aptina_mt9v113.dat:system/usr/share/camera/config_file/aptina_mt9v113.dat \
-    vendor/sony/nypon/proprietary/usr/share/camera/config_file/aptina_mt9v115.dat:system/usr/share/camera/config_file/aptina_mt9v115.dat \
-    vendor/sony/nypon/proprietary/usr/share/camera/config_file/omnivision_ov5640_pri.dat:system/usr/share/camera/config_file/omnivision_ov5640_pri.dat \
-    vendor/sony/nypon/proprietary/usr/share/camera/config_file/omnivision_ov5640_pri_30fps.dat:system/usr/share/camera/config_file/omnivision_ov5640_pri_30fps.dat \
-    vendor/sony/nypon/proprietary/usr/share/camera/config_file/omnivision_ov5640_sec.dat:system/usr/share/camera/config_file/omnivision_ov5640_sec.dat \
-    vendor/sony/nypon/proprietary/usr/share/camera/config_file/sony_isx006.dat:system/usr/share/camera/config_file/sony_isx006.dat \
     vendor/sony/nypon/proprietary/usr/share/camera/tuning/ChrLin-020111.nmx:system/usr/share/camera/tuning/ChrLin-020111.nmx \
     vendor/sony/nypon/proprietary/usr/share/camera/tuning/ChrLin-030111.nmx:system/usr/share/camera/tuning/ChrLin-030111.nmx \
     vendor/sony/nypon/proprietary/usr/share/camera/tuning/ChrLsc-020111.nmx:system/usr/share/camera/tuning/ChrLsc-020111.nmx \
@@ -98,7 +54,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nypon/proprietary/usr/share/camera/tuning/TunVidPre-030111.xml:system/usr/share/camera/tuning/TunVidPre-030111.xml \
     vendor/sony/nypon/proprietary/usr/share/camera/tuning/TunVidRec-020111.xml:system/usr/share/camera/tuning/TunVidRec-020111.xml \
     vendor/sony/nypon/proprietary/usr/share/camera/tuning/TunVidRec-030111.xml:system/usr/share/camera/tuning/TunVidRec-030111.xml \
-    vendor/sony/nypon/proprietary/vendor/camera/APT00YP1.dat:system/vendor/camera/APT00YP1.dat \
     vendor/sony/nypon/proprietary/vendor/camera/flash.dat:system/vendor/camera/flash.dat \
     vendor/sony/nypon/proprietary/vendor/camera/KMO08BN0.dat:system/vendor/camera/KMO08BN0.dat \
     vendor/sony/nypon/proprietary/vendor/camera/KMO08BN0_BU6456.dat:system/vendor/camera/KMO08BN0_BU6456.dat \
